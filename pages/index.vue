d82da36df24205b350e87ebc5bf113021134daf4
lhtmdy <lhtmdy@gmail.com>
2026-04-28 16:57:19 +0800

    update info

--- pages/index.vue
+++ pages/index.vue
@@ -13,6 +13,7 @@ const seo = ref({
   title: "李欣恬 | 前端工程師",
   description: '前端工程師-李欣恬 Mandy Li 的個人網站',
   // image: 'https://mandy2000.com/image/mandy_logo.png',
+  image: 'https://lhtmdy.github.io/profile2026/image/mandy_logo.png',
 })
 useMetaHead({
   ...seo.value,
